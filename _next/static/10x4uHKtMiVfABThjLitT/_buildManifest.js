87041d4fa1cbd54538e248045e805b6abd2c2cfa
quuququ <123919827+quuququ@users.noreply.github.com>
2023-10-29 13:58:06 +0000

    Deploying to gh-pages from @ quuququ/quuququ@13418fe87cc694fd7660edff595a1231b2b20217 🚀

---
+++ _next/static/10x4uHKtMiVfABThjLitT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/e7d3bab42d9af29d.css","static/chunks/pages/index-6bde8b730ef00bb4.js"],"/_error":["static/chunks/pages/_error-e515def81245dfa4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
