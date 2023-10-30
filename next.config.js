b9d7a538a5b9d704dcf8ae68432feefc17163503
quuququ <123919827+quuququ@users.noreply.github.com>
2023-10-30 11:44:09 +0000

    Deploying to gh-pages from @ quuququ/quuququ@a1ca25980b23383b35da5b74c6b8073e71958192 🚀

--- next.config.js
+++
@@ -1,3 +0,0 @@
-// Default Pages configuration for Next
-const nextConfig = {images: {unoptimized: true},experimental: {images: {unoptimized: true}},basePath: "/quuququ"}
-module.exports = nextConfig
